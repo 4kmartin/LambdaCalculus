a3b28cef2035d404b58463c54231fd1ac9eaa5ad
Anthany Martin <4kmartin@gmail.com>
2022-04-13 13:27:56 -0500

    added comments

--- pairs.py
+++ pairs.py
@@ -5,7 +5,7 @@ PAIR = lambda x, y: lambda f: f(x)(y) # a pair is a tuple of two elements
 
 CAR = lambda p: p(true) # return the first element or Contents of the "A" Register
 CDR = lambda p: p(false) # return the last element or Contents of the "D" Register
-REASSIGN = lambda p, new, fe: fe(PAIR(new,CDR(p)))(PAIR(CAR(p),new))
+REASSIGN = lambda p, new, fe: fe(PAIR(new,CDR(p)))(PAIR(CAR(p),new)) # takes a PAIR, new value, and a boolean to indicate weather to replace the first element (false would replace the seccond)
 
 toTuple = lambda p: (CAR(p),CDR(p))
 
