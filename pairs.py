1bd5eac2c0319b169bda97ab6dc57ef33337e9cc
Anthany Martin <4kmartin@gmail.com>
2022-04-13 13:03:57 -0500

    added some needed functions to pairs

--- pairs.py
+++ pairs.py
@@ -1,13 +1,13 @@
-from boolean import true, false, IF
+from boolean import true, false
 from numerals import zero, increment
 
 PAIR = lambda x, y: lambda f: f(x)(y) # a pair is a tuple of two elements
 
 CAR = lambda p: p(true) # return the first element or Contents of the "A" Register
 CDR = lambda p: p(false) # return the last element or Contents of the "D" Register
+REASSIGN = lambda p, new, fe: fe(PAIR(new,CDR(p)))(PAIR(CAR(p),new))
+
+toTuple = lambda p: (CAR(p),CDR(p))
 
 NIL = true
 NULL = lambda p: p(lambda x: lambda y: false) # checks if a pair is empty
-
-
-isPAIR = lambda p: p(lambda x: lambda y: true)
